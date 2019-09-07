59595b63b64e26eb3f373072eed7efb64dce544f
lchoo <lchoo1996@gmail.com>
2019-09-07 15:28:38 -0700

    Updates

---
+++ precache-manifest.e5a0a632ae92cabc2e562d216d580a70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9799be9884bfd1430776f8bfe8b3af7e",
+    "url": "/ARHololens/index.html"
+  },
+  {
+    "revision": "641bfe85058a3ea08685",
+    "url": "/ARHololens/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "cd1c0a780a7455a04968",
+    "url": "/ARHololens/static/js/2.79819f37.chunk.js"
+  },
+  {
+    "revision": "641bfe85058a3ea08685",
+    "url": "/ARHololens/static/js/main.447383a3.chunk.js"
+  },
+  {
+    "revision": "660ce85bf3a2c7da79aa",
+    "url": "/ARHololens/static/js/runtime~main.a84e7ed7.js"
+  }
+]);
